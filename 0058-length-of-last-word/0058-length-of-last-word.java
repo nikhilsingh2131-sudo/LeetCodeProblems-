036f22b692271794977c32262892530ccff835b9
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-07-30 22:30:58 +0530

    Time: 0 ms (100%), Space: 42.7 MB (92.94%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,25 +1,19 @@
-import java.util.Stack;
-
 class Solution {
     public int lengthOfLastWord(String s) {
 
-        Stack<Character> st = new Stack<>();
+        int i = s.length() - 1;
 
-        for (char ch : s.toCharArray()) {
-            st.push(ch);
-        }
-
-        // Remove trailing spaces
-        while (!st.isEmpty() && st.peek() == ' ') {
-            st.pop();
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
         }
 
         int count = 0;
 
         // Count last word
-        while (!st.isEmpty() && st.peek() != ' ') {
-            st.pop();
+        while (i >= 0 && s.charAt(i) != ' ') {
             count++;
+            i--;
         }
 
         return count;
