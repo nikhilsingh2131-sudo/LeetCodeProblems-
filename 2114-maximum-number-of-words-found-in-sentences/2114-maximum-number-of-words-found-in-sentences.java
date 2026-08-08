b25468c3479c0d811d10157065e2b7c33fad69eb
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-08-08 13:06:42 +0530

    Time: 3 ms (87.05%), Space: 46.2 MB (25.55%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+
+        int max = 0;
+
+        for (String sentence : sentences) {
+
+            int words = sentence.split(" ").length;
+
+            max = Math.max(max, words);
+        }
+
+        return max;
+    }
+}
