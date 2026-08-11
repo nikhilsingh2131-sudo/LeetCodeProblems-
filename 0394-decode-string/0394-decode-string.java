28233340b56113617d75b98f5ce6adaf8d34fa61
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-08-12 00:30:15 +0530

    Time: 3 ms (22.18%), Space: 43 MB (39.92%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,55 +1,34 @@
 class Solution {
     public String decodeString(String s) {
+        Stack<Integer> number = new Stack<>();
+        Stack<String> string = new Stack<>();
 
-        Stack<Integer> st1 = new Stack<>();
-        Stack<String> st2 = new Stack<>();
+        String current ="";
+        int n =0;
 
-        int num = 0;
+        for(char ch : s.toCharArray()){
+            if (Character.isDigit(ch)){
+                n = n*10 +(ch - '0');
+            }else if(ch == '['){
+                number.push(n);
+                string.push(current);
+                current ="";
+                n=0;
+            }else if(ch==']'){
+                String prev = string.pop();
+                int count = number.pop();
+                String temp ="";
 
-        for (char ch : s.toCharArray()) {
-
-            if (Character.isDigit(ch)) {
-
-                num = num * 10 + (ch - '0');
-
-            } else if (ch == '[') {
-
-                st1.push(num);
-                num = 0;
-                st2.push("[");
-
-            } else if (Character.isLetter(ch)) {
-
-                st2.push(String.valueOf(ch));
-
-            } else if (ch == ']') {
-
-                StringBuilder curr = new StringBuilder();
-
-                while (!st2.isEmpty() && !st2.peek().equals("[")) {
-                    curr.insert(0, st2.pop());   // reverse order maintain
+                for(int i =0 ; i<count ;i++){
+                    temp += current ;
                 }
-
-                st2.pop(); // remove "["
-
-                int repeat = st1.pop();
-
-                StringBuilder temp = new StringBuilder();
-
-                for (int i = 0; i < repeat; i++) {
-                    temp.append(curr);
-                }
-
-                st2.push(temp.toString());
+                current = prev+temp;
+            }else{
+                current +=ch;
             }
+
         }
-
-        StringBuilder ans = new StringBuilder();
-
-        while (!st2.isEmpty()) {
-            ans.insert(0, st2.pop());
-        }
-
-        return ans.toString();
+        return current;
+        
     }
 }
