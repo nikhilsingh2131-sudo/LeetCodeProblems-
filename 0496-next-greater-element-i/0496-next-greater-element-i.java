3e9239ba5a2d7905ffb7d2bfc1fb26826c5e6865
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-08-12 21:19:31 +0530

    Time: 5 ms (15.39%), Space: 44.9 MB (81.14%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,27 +1,49 @@
-import java.util.*;
-
 class Solution {
-
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        Stack<Integer> st = new Stack<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] nextGreaterElements = new int[nums2.length];
 
-        for(int num :nums2){
-            while(!st.isEmpty() && num > st.peek()){ 
-            map.put(st.pop() ,num);
+        Stack<Integer> helperStack = new Stack<>();
+
+        // nums2 ko right se left traverse karenge
+        for (int i = nums2.length - 1; i >= 0; i--) {
+
+            int element = nums2[i];
+
+            // Chhote ya equal elements ko remove karo
+            while (!helperStack.isEmpty() &&
+                   helperStack.peek() <= element) {
+
+                helperStack.pop();
+            }
+
+            // Stack empty hai -> koi greater element nahi
+            if (helperStack.isEmpty()) {
+                nextGreaterElements[i] = -1;
+            }
+            // Stack ka top hi next greater element hai
+            else {
+                nextGreaterElements[i] = helperStack.peek();
+            }
+
+            // Current element ko stack mein daalo
+            helperStack.push(element);
         }
-        st.push(num);
-    }
 
-     while(!st.isEmpty()){
-        map.put(st.pop() ,-1);
+        // nums1 ke answers
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+
+            for (int j = 0; j < nums2.length; j++) {
+
+                if (nums1[i] == nums2[j]) {
+                    ans[i] = nextGreaterElements[j];
+                    break;
+                }
+            }
+        }
+
+        return ans;
     }
-     
-     int [] ans = new int[nums1.length];
-    for(int i=0 ; i< nums1.length ; i++){
-       ans[i] = map.get(nums1[i]);
-    }
-    return ans;
-  }
 }
