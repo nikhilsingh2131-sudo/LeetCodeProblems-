08582b9b6bf08ff50b7e709548bedb12c99fe806
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-08-12 21:43:05 +0530

    Time: 4 ms (74.23%), Space: 45.2 MB (62.22%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,47 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        int[] nextGreaterElements = new int[nums2.length];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
 
-        Stack<Integer> helperStack = new Stack<>();
+        // nums2 ko left se right traverse karo
+        for (int num : nums2) {
 
-        // nums2 ko right se left traverse karenge
-        for (int i = nums2.length - 1; i >= 0; i--) {
-
-            int element = nums2[i];
-
-            // Chhote ya equal elements ko remove karo
-            while (!helperStack.isEmpty() &&
-                   helperStack.peek() <= element) {
-
-                helperStack.pop();
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
             }
 
-            // Stack empty hai -> koi greater element nahi
-            if (helperStack.isEmpty()) {
-                nextGreaterElements[i] = -1;
-            }
-            // Stack ka top hi next greater element hai
-            else {
-                nextGreaterElements[i] = helperStack.peek();
-            }
-
-            // Current element ko stack mein daalo
-            helperStack.push(element);
+            stack.push(num);
         }
 
         // nums1 ke answers
         int[] ans = new int[nums1.length];
 
         for (int i = 0; i < nums1.length; i++) {
-
-            for (int j = 0; j < nums2.length; j++) {
-
-                if (nums1[i] == nums2[j]) {
-                    ans[i] = nextGreaterElements[j];
-                    break;
-                }
-            }
+            ans[i] = map.getOrDefault(nums1[i], -1);
         }
 
         return ans;
