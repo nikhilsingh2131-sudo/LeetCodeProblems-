b88d2b07c0201089cd82ad9794fedb6c77f41099
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-08-26 03:24:21 +0530

    Time: 3 ms (87.96%), Space: 52.4 MB (32.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,27 +2,27 @@ class Solution {
 
     public int numIslands(char[][] grid) {
 
-        // Total rows
+    
         int rows = grid.length;
 
-        // Total columns
+      
         int cols = grid[0].length;
 
-        // Number of islands
+      
         int islands = 0;
 
-        // Har cell ko check karenge
+       
         for (int i = 0; i < rows; i++) {
 
             for (int j = 0; j < cols; j++) {
 
-                // Agar current cell land hai
+             
                 if (grid[i][j] == '1') {
 
-                    // Naya island mila
+                    
                     islands++;
 
-                    // Is poore island ko visit kar do
+                   
                     dfs(grid, i, j);
                 }
             }
@@ -33,7 +33,7 @@ class Solution {
 
     public void dfs(char[][] grid, int r, int c) {
 
-        // Boundary ke bahar chale gaye
+        
         if (r < 0 || c < 0 ||
             r >= grid.length ||
             c >= grid[0].length) {
@@ -41,26 +41,16 @@ class Solution {
             return;
         }
 
-        // Water hai ya already visit ho chuka
-        if (grid[r][c] == '0') {
+    
+       if(grid[r][c] =='0'){
+        return;
+       }
 
-            return;
-        }
+       grid[r][c]='0';
 
-        // Current land ko water bana diya
-        // Iska matlab visit ho gaya
-        grid[r][c] = '0';
-
-        // Upar
-        dfs(grid, r - 1, c);
-
-        // Neeche
-        dfs(grid, r + 1, c);
-
-        // Left
-        dfs(grid, r, c - 1);
-
-        // Right
-        dfs(grid, r, c + 1);
+       dfs(grid , r , c-1);
+       dfs(grid , r , c+1);
+       dfs(grid , r-1 ,c);
+       dfs(grid , r+1 , c);
     }
 }
