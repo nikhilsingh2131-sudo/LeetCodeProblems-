cfdf34af80c79b40f873826ea823e73d36cc76ce
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-08-25 21:45:16 +0530

    Time: 15 ms (55.13%), Space: 47.5 MB (67.35%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,24 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        HashMap<Integer , Integer>map = new HashMap<>();
+        HashMap<Integer , Integer> map = new  HashMap<>();
 
-        for(int num:nums){
-            map.put(num , map.getOrDefault(num ,0)+1);
+        for(int num : nums){
+            map.put(num , map.getOrDefault(num ,  0)+1);
         }
 
-        PriorityQueue<Integer> pq =
-    new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
+        
+        for (int num : map.keySet()) {
+            pq.add(num);
+        }
 
-    pq.addAll(map.keySet());
+        int[] ans = new int[k];
 
-    int [] ans = new int[k];
+        for(int i =0 ; i<k ; i++){
 
-    for(int i=0 ;i< k;i++){
-        ans[i] = pq.poll();
-    }
+            ans[i] = pq.poll();
+            
+        }
 
-    return ans;
+        return ans;
         
     }
 }
