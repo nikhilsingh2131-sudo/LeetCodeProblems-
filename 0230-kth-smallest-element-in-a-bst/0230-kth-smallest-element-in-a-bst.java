cf92bd334388c2d9a1a2deb3e91cedc0a7bc84d6
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-09-13 00:16:07 +0530

    Time: 5 ms (3.5%), Space: 46.3 MB (94.56%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,23 +14,32 @@
  * }
  */
 class Solution {
-    PriorityQueue<Integer> pq = new PriorityQueue<>();
     public int kthSmallest(TreeNode root, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        smallest(root);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
 
-     for(int i =1;i<k ;i++){
-        pq.poll();
-     }
-     return pq.peek();
-        
-    }
-    public void smallest(TreeNode root ){
-        if(root==null) return ;
+        while(!q.isEmpty()){
+            int size = q.size();
 
-        pq.add(root.val);
+            for(int i =0 ; i<size ; i++){
+                TreeNode node = q.poll();
+                pq.add(node.val);
 
-        smallest(root.left);
-        smallest(root.right);
+                if(node.left!=null){
+                    q.offer(node.left);
+                }
+
+                if(node.right!=null){
+                    q.offer(node.right);
+                }
+            }
+        }
+
+        for(int i =0 ; i<k-1 ; i++){
+          pq.poll();
+        }
+        return pq.peek();
     }
 }
