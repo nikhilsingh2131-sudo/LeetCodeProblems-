39a08d2c22b135f565537e071233af884225f36c
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-09-13 01:31:57 +0530

    Time: 3 ms (87.44%), Space: 52.4 MB (45.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,56 +1,34 @@
 class Solution {
-
     public int numIslands(char[][] grid) {
 
-    
-        int rows = grid.length;
+        int n = grid.length ;
+        int m = grid[0].length ;
+        int island=0;
 
-      
-        int cols = grid[0].length;
-
-      
-        int islands = 0;
-
-       
-        for (int i = 0; i < rows; i++) {
-
-            for (int j = 0; j < cols; j++) {
-
-             
-                if (grid[i][j] == '1') {
-
-                    
-                    islands++;
-
-                   
-                    dfs(grid, i, j);
+        for(int i =0  ; i< n ; i++){
+            for(int j=0 ; j<m ; j++){
+                if(grid[i][j]=='1'){
+                    island++;
+                    dfs(grid , i , j);
                 }
             }
         }
+        return island;
+    } public void dfs(char[][]grid , int r , int c){
+       
 
-        return islands;
-    }
-
-    public void dfs(char[][] grid, int r, int c) {
-
-        
-        if (r < 0 || c < 0 ||
-            r >= grid.length ||
-            c >= grid[0].length) {
-
+        if(r<0 ||c<0 || r>=grid.length || c>=grid[0].length){
             return;
         }
+        if(grid[r][c]=='0'){
+            return ;
+        }
 
-    
-       if(grid[r][c] =='0'){
-        return;
-       }
+        grid[r][c] ='0';
 
-       grid[r][c]='0';
-
-       dfs(grid , r , c-1);
-       dfs(grid , r , c+1);
-       dfs(grid , r-1 ,c);
-       dfs(grid , r+1 , c);
+        dfs(grid ,r+1 , c);
+        dfs(grid , r-1 ,c);
+        dfs(grid ,r, c+1);
+        dfs(grid , r , c-1);
     }
 }
