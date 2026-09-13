ee9b81e957afee4b32e8fc2ef1c9f6dbcdad33c2
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-09-13 23:35:41 +0530

    Time: 69 ms (9.55%), Space: 48.5 MB (11.36%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+
+        int n = hand.length ; 
+
+        if(n%groupSize != 0){
+            return false ;
+        }
+
+      TreeMap<Integer ,Integer> map = new TreeMap<>();
+
+      for(int num : hand){
+        map.put(num , map.getOrDefault(num ,0)+1);
+      }
+
+      while(!map.isEmpty()){
+        int first = map.firstKey();
+
+        for(int i=0 ;i<groupSize ; i++){
+            int card = first+i;
+
+            if(!map.containsKey(card)){
+                return false;
+            }
+
+            map.put(card , map.getOrDefault(card , 0)-1);
+            if(map.get(card)==0){
+                map.remove(card);
+            }
+        }
+      }
+        return true;
+    }
+}
