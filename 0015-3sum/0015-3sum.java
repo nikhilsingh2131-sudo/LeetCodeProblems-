d6acdc32b621efbdd69ae872d14f35714c9e49e9
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-09-16 02:50:01 +0530

    Time: 34 ms (57.67%), Space: 59 MB (74.55%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,34 +5,42 @@ class Solution {
 
         Arrays.sort(nums);
 
-        for(int i =0 ; i<nums.length ; i++){
+        for(int i=0; i< nums.length ;i++){
+            
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int j = i+1 ;
-            int k = nums.length-1;
 
-            while(j<k){
-               
+            int left = i+1;
+            int right = nums.length -1;
 
-                int sum = nums[i]+nums[j]+nums[k];
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
 
                 if(sum==0){
-                   ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                   j++;
-                   k--;
+                    ans.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
+                    left++;
+                    right--;
 
-                   while(j<k && nums[j]==nums[j-1]) j++;
+                    while(left<right && nums[left]==nums[left-1]){
+                        left++;
+                    }
+                      while(left< right && nums[right]==nums[right+1]){
+                        right--;
+                    }
 
-                    while(j<k && nums[k]== nums[k+1]) k--;
 
-
+                    
                 }else if(sum>0){
-                    k--;
+                    right--;
                 }else{
-                    j++;
+                    left++;
                 }
             }
         }
+
+       
         return ans ;
     }
 }
