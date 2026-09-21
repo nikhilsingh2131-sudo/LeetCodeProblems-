4f24d518bf35efa1dd6e1a8583306fcf6c8ec8c7
Nikhil Kumar <nikhilsingh2131@gmail.com>
2026-09-22 01:33:27 +0530

    Time: 0 ms (100%), Space: 43.5 MB (56.96%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int index =0;
+    public TreeNode bstFromPreorder(int[] preorder) {
+
+        return build(preorder , Integer.MIN_VALUE , Integer.MAX_VALUE);
+        
+    }public TreeNode build(int[]preorder , int low , int high){
+        if(index==preorder.length){
+            return null;
+        }
+
+        if (preorder[index] < low || preorder[index] > high) {
+            return null;
+        }
+
+        int value = preorder[index];
+        index++;
+
+        TreeNode root = new TreeNode(value);
+
+        root.left = build(preorder , low , value);
+        root.right = build(preorder , value, high);
+
+        return root;
+    }
+}
